026c36f1cb54e7b75d52fd4acd49d346b4f9e35c
Rodney R <majid.muhassan@gmail.com>
2017-12-12 03:26:27 +0000

    the survey page is broken again.  Will have to rework the logic.

--- app/routing/apiRoutes.3.js
+++ app/routing/apiRoutes.3.js
@@ -15,13 +15,8 @@ function initRoutes(app) {
     });
     //* A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function(req, res) {
-        var newextroverts = req.body;
+        var newextroverts = req.body.score;
         var newscore = [];
-        var yourFriend = {
-            name: "",
-            photo: "",
-
-        };
         var bestMatch = Infinity;
         console.log("test test test test  " + JSON.stringify(newextroverts));
         //first loop through friends.
@@ -29,19 +24,25 @@ function initRoutes(app) {
             // totalDiff will hold the scores and be used in the math later on
             var totalDiff = 0;
             //time to compare friends
-            for (var j = 0; j < newextroverts.length; j++) {
+            for (var j = 0; j < newextroverts.length; i++) {
                 totalDiff += (Math.abs(parseInt(extroverts[i].scores[j]) - parseInt(newextroverts[j])));
-            } //truthty work around
-            if (totalDiff <= yourFriend.bestMatch) {
-
-                yourFriend.name = newextroverts.name;
-                yourFriend.photo = newextroverts.photo;
-                yourFriend.bestMatch = totalDiff
             }
-            extroverts.push(newextroverts);
+            newscore.push(totalDiff);
         }
-        //truthty work around
-        res.JSON(yourFriend);
+        //running into a truthy issue
+        // ====================================
+        // var score = newextroverts.score;
+        // console.log("this should just be score " + JSON.stringify(newscore));
+        newextroverts.name = newextroverts.name.replace(/\s+/g, "").toLowerCase();
+        for (var i = 0; i < newextroverts.length; i++) {
+            console.log("new extroverts list " + newextroverts[i]);
+
+
+
+
+        }
+        extroverts.push(newextroverts);
+        res.json(newextroverts);
     });
 
 }
